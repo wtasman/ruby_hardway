ff9d275795305b9c731e6a34f3d9815a3f2ba3bc
William Asman <wtasman@Williams-MacBook-Pro.local>
2017-07-31 11:57:17 -0700

    pushing completed exercises

---
+++ ex2/ex2.rb
@@ -0,0 +1,9 @@
+# Yay comments!! To whom it may concern:
+# Comments let me leave little notes in my code
+
+puts "You should comment your code" #reallifeadvice
+
+#I like to use comments to disable stupid parts of my code
+#puts "Why doesn't this work'
+
+puts "At least this works"
---
+++ ex3/my_rent.rb
@@ -0,0 +1,5 @@
+income = 200.0
+days_worked = 16.0
+rent = 1000.0
+puts "You need to save $#{rent/days_worked} per day you work to pay for rent."
+puts "That is aproximately #{(rent / (days_worked * income)) * 100}% of your income"
