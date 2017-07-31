103ffc3cd9752139d3c8316c2cb1acfa00287c09
William Asman <wtasman@hotmail.com>
2017-07-31 13:42:52 -0700

    I believe I have completed a few more exercises

---
+++ ex11/ex11.rb
@@ -0,0 +1,11 @@
+print "What is your name? "
+name = gets.chomp
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+
+puts "#{name}, you're #{age} years old, #{height} tall and #{weight} heavy." 
---
+++ ex9/ex9.rb
@@ -0,0 +1,16 @@
+# Here's some strange stuff, remember to type it exactly.
+#Initializes days to a string containing 3 letter capped days of the week
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#Initializes months to a string of the first 8 months seperated by newlines
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#puts the days
+puts "Here are the days: #{days}"
+#puts the months
+puts "Here are the months: #{months}"
+#puts a string where the newlines are printed
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as as we like.
+Even 4 LInes if we want, or 5, or 6.
+}
